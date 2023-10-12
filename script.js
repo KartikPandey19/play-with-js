e62b82283012b24674daaec0730b8a696140f808
KartikPandey19 <kartik2000pandey@gmail.com>
2023-10-12 19:51:58 +0530

    Guessing game

---
+++ script.js
@@ -0,0 +1,72 @@
+let randomNumber = Math.floor(Math.random()*100);
+console.log(randomNumber);
+
+const lastGuess = document.querySelector(".lastGuess");
+const help = document.querySelector(".help");
+const result = document.querySelector(".result");
+
+
+const submitBtn = document.querySelector(".submitBtn");
+const guessField = document.querySelector(".guessField");
+
+
+let guessCount = 1;
+let resetButton;
+
+submitBtn.addEventListener('click',checkGuess)
+
+function checkGuess(){
+    const userGuess = Number(guessField.value);
+    if(userGuess===randomNumber){
+        result.textContent="Congratulation! You got it right";
+        result.style.backgroundColor="green";
+        help.textContent="";
+        setGameOver();
+    }else if(guessCount===10){
+        result.textContent="!!!GAME OVER";
+        help.textContent="";
+        setGameOver();
+    }else{
+        result.textContent="oops! Wrong";
+        result.style.backgroundColor="red";
+        if(userGuess<randomNumber){
+            help.textContent="Last Guess was too low!";  
+        }else{
+            help.textContent="Last Guess was too high!";  
+
+        }
+    }
+    guessCount++;
+    guessField.value="";
+    guessField.focus();  
+}
+
+function setGameOver(){
+    guessField.disabled ="true";
+    submitBtn.disabled = "true";
+    resetButton = document.createElement("button");
+    resetButton.textContent = "Start new Game";
+    document.body.append(resetButton);
+    resetButton.addEventListener("click",resetGame);
+   
+}
+
+function resetGame(){
+    guessCount=1;
+
+    const resetParas = document.querySelectorAll(".resultArea p");
+    for(const resetPara of resetParas){
+        resetPara.textContent=""
+    }
+    resetButton.parentNode.removeChild(resetButton);
+
+    guessField.disabled =false;
+    submitBtn.disabled = false;
+    guessField.value="";
+    guessField.focus();
+
+    result.style.backgroundColor="white";
+
+    randomNumber = Math.floor(Math.random()*100);
+
+}
